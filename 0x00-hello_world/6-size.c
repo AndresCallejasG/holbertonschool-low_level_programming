e08bddcb0943c513900f1528ae2f49edfb7df4c7
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-05 20:11:27 +0000

    text fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char pointer: %d\n", sizeof(char *));
-printf("Size of a int pointer: %d\n", sizeof(int *));
-printf("Size of a float pointer: %d\n", sizeof(float *));
-printf("Size of a long int pointer: %d\n", sizeof(long int *));
-printf("Size of a double pointer: %d\n", sizeof(double *));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
