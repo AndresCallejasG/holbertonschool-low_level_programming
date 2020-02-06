cf5f82e048a01ec0fb6ed5e28eb48fb32f5cf5b6
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-06 20:55:14 +0000

    print different combinations with 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - prints number from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		j=i+1;
+		while (j<=9)
+		{
+			putchar((char) (i + 48));
+			putchar((char) (j + 48));
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		j++;
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
