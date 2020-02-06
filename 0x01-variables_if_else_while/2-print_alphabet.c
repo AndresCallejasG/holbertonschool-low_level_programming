76263e6228a1c1af57eb46c9cb3ef0a0099631cf
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-06 17:43:21 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Checks last digit of a rnd num
+ * Return: 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
