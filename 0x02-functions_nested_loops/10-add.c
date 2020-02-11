e1d6e04a2ab5ed598889cb5d7c3f11a5274b1084
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-11 19:29:02 +0000

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* add - add 2 integers and return the value.
+*@a: first number
+*@b: second number
+* Return: sum
+*/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+
+}
