412e218cc3a025f67da147a4ddaf1c895656e13f
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-11 00:45:12 +0000

    prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_alphabet - print alphabet to stdout
+*
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
