70a040174be2c97ad3f8e1c3e440eab420819e08
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-11 14:29:44 +0000

    betty checked

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 void print_alphabet_x10(void)
 {
 	int i, j;
-	
-	for(j = 0; j < 10; j++)
+
+	for (j = 0; j < 10; j++)
 	{
-		for (i = 'a'; i <= 'z'; i++)		
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
-	}	
+	}
 }
