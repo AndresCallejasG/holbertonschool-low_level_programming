b177f827df67e2dce1f7cbd6e3c2c6574ed94bd1
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-12 04:30:45 +0000

    sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - sum of all the multiples of 3 or 5 below 1024
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j, sum_i, sum_j, sum_ij;
+
+	for (i = 0; i < 1024; i += 3)
+	{
+		sum_i += i;
+	}
+	for (j = 0; j < 1024; j += 3)
+	{
+		if (!(j % 5  == 0 || j % 10 == 0))
+			sum_j += j;
+	}
+
+	sum_ij = sum_i + sum_j;
+	printf("%d\n", sum_ij);
+}
