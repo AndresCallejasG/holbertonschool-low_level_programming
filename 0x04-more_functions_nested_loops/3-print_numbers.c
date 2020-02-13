0f6b11e4f051af6cf42fb413fdbfbc649d7e9ed4
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-13 17:03:52 +0000

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_numbers - print from 0 to 9 to stdout
+*
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
