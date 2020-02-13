937c1d16d3d029ecb77fb228fd08185cb4c13aa6
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-13 16:31:18 +0000

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*mul - multiply 2 integers and return the value.
+*@a: first number
+*@b: second number
+* Return: mult
+*/
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+
+}
