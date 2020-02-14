2c5cfa29f68eedefc05a3daafd6eb8df2a4b0e58
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-14 03:11:47 +0000

    MAX_VALUE int fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
 * print_number - prints an integer char by char
 * @n: The integer to print
@@ -23,19 +22,16 @@ void print_number(int n)
 	}
 
 	num_temp = num;
-
 	while (num_temp != 0)
 	{
 		size++;
 		num_temp /= 10;
 
 	}
-
-	for  i = 1; i < size; i++)
+	for  (i = 1; i < size; i++)
 	{
 		div = div * 10;
 	}
-
 	for (i = size - 1; i >= 1; i--)
 	{
 		dig = num / div;
