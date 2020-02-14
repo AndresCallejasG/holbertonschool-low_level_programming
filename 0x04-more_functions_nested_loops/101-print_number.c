91164ec7e3f8500b0d12679033f2ea8752c3aa45
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-14 00:00:17 +0000

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* print_number - prints an integer char by char
+* @n: The integer to print
+*
+* Return: void
+*/
+
+void print_number(int n)
+{
+	int i, size;
+	int num, num_temp, dig, div;
+
+	num = n;
+	size = 0;
+	div = 1;
+
+	if (num < 0)
+	{
+		num = num * -1;
+		_putchar('-');
+	}
+	num_temp = num;
+	while (num_temp != 0)
+	{
+		size++;
+		div = div * 10;
+		num_temp /= 10;
+	}
+	div /= 10;
+	for (i = size - 1; i >= 1; i--)
+	{
+		dig = num / div;
+		_putchar((dig % 10) + '0');
+		div /= 10;
+	}
+	_putchar((num % 10) + '0');
+}
