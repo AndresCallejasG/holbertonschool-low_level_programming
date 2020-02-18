4a9a1a466445ce5de765ac460b9bef54dbd7a4da
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-18 03:50:11 +0000

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* rev_string - reverses a string.
+* @s: string
+*
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i, j, k;
+	char str[10];
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		str[k] = s[j];
+		k++;
+
+	}
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+}
