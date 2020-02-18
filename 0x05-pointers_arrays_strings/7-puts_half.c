6707d7b92370d86e70f45717fe1b96f8f11e83b8
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-18 05:15:43 +0000

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+* puts_half - that prints half of a string, followed by a new line
+* @str: string
+*
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i, length, middle;
+
+	i = 0;
+	length = 0;
+	middle = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		middle = length / 2;
+	}
+	else
+	{
+		middle = (length - 1) / 2;
+	}
+	for (i = middle; i < length; i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+}
