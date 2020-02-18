6eede87db37d2dbf3ba5d8bbaa6ca7ad8dce5891
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-18 01:55:42 +0000

    takes an int pointer and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - takes a pointer to an int as parameter
+* and updates the value it points to to 98.
+* @n: pointer to the variable to change
+*
+* Return: uppercase 1.
+* 0 otherwise.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
