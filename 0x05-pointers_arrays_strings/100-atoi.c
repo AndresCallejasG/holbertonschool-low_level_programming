1693842e15206519705bea24f105b7ff3dabd373
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-19 01:56:48 +0000

    new logic, just 1 loop

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,12 @@
 
 int _atoi(char *s)
 {
-	int i, j;
-	int neg, mult, num, first;
+	int i;
+	int neg, num, flag;
 
 	i = 0;
-	j = 0;
 	neg = 0;
-	mult = 1;
+	flag = 0;
 	num = 0;
 
 	while (s[i] != '\0')
@@ -25,20 +24,16 @@ int _atoi(char *s)
 			neg++;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			first = i;
-			while ((s[i + 1] != '\0') && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-				i++;
-			for (j = i; j >= first; j--)
-			{
-				num += (s[j] - '0') * mult;
-				mult *= 10;
-			}
-			if ((neg % 2) == 1)
-				num = num * -1;
-			return (num);
+			flag = 1;
+			num *= 10;
+			num += s[i] - '0';
 		}
+		else if (flag)
+			break;
 		i++;
 	}
+	if ((neg % 2) == 1)
+		num = num * -1;
+	return (num);
 
-	return (0);
 }
