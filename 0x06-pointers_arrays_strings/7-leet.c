63f356d79af8675eca3f2b8336c6e0e067dfad74
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-20 23:48:57 +0000

    no external functions

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,6 @@ char *leet(char *s)
 {
 	int i, j;
 	char leet[] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T', '\0'};
-	char aux[] = {'\0', '\0'};
 
 	i = 0;
 	j = 0;
@@ -21,10 +20,11 @@ char *leet(char *s)
 	{
 		while (j < 8)
 		{
-			aux[0] = s[i];
-			string_toupper(aux);
-			if ((aux[0] == leet[j]) && (s[i] != '\0'))
+			if (((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) &&
+			 (s[i] == leet[j] || s[i] == leet[j] + 32) && (s[i] != '\0'))
+			{
 				s[i] = j + '0';
+			}
 			j++;
 		}
 		i++;
