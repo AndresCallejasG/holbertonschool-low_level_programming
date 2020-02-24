b88f2b32c9629b476886f947b2b878c1f29ffab1
andrescallejasg <andres.callejasg@gmail.com>
2020-02-24 16:56:51 +0000

    fills the first n bytes of s with b

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b
+ * @s: pointer to memory
+ * @b: byte to fill.
+ * @n: first n bytes
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
