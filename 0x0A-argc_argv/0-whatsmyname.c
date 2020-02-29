d720ea319b7b21834f46c7318db899e0fd2c0a0b
Andres Callejas G <andres.callejasg@gmail.com>
2020-02-29 00:18:10 +0000

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints its name
+ *
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: pointer to memory area s
+ */
+int main(int  __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
