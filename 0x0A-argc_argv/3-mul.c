fd65a185d1944dfbfba7ee4b278f3be848d6d34c
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-01 18:04:00 +0000

    multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives.
+ *
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	
+	if ( argc == 3)
+	{
+		result = (atoi(argv[1])) * (atoi(argv[2]));
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
