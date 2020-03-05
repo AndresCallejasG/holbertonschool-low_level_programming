43f1c67043dfadf2fcb4f25413abf9dd78f22693
andrescallejasg <andres.callejasg@gmail.com>
2020-03-05 18:05:46 +0000

    simulates calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _memset - fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b
+ * @s: pointer to memory
+ * @b: byte to fill.
+ * @n: first n bytes
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory using malloc.
+ *
+ * @nmemb: sizeof type
+ * @size: number of items
+ * Return: Always 0.
+ * if fails null
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (0);
+	mem = _memset(mem, '0', size);
+	return (mem);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,4 +4,6 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int len(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
 #endif
