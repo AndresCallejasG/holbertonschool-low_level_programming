212f2396ffe09ea8c7077d6c13a295f3e483c187
andrescallejasg <andres.callejasg@gmail.com>
2020-03-05 20:29:43 +0000

    simulate realloc()

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ *
+ * @ptr: old pointer
+ * @old_size: size of ptr
+ * @new_size: in bytes, the\
+ * new memory block size
+ * Return: Always 0.
+ * if fails null
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	char *t_ptr;
+	char *t2_ptr;
+	unsigned int i;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == 0)
+		return (0);
+	t_ptr = new_ptr;
+	t2_ptr = ptr;
+	for (i = 0; i < new_size; i++)
+	{
+		t_ptr[i] = t2_ptr[i];
+	}
+	free(ptr);
+	return (new_ptr);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -7,4 +7,5 @@ unsigned int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
