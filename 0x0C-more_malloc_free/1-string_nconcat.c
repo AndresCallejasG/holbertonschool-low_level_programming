b11f960114a73093d2b30e0f83424f33461ce148
andrescallejasg <andres.callejasg@gmail.com>
2020-03-05 17:32:10 +0000

    concat s1 with n bytes of s2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_nconcat - concatenate two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes from s2 to copy
+ *
+ * Return: array concatenated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int i, j, len1, len2;
+
+	i = 0;
+	j = 0;
+
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+
+	len1 = len(s1);
+	len2 = len(s2);
+
+	concat = malloc((len1 + len2) * sizeof(char) + 1);
+
+	if (concat == 0)
+		return ('\0');
+
+	for (i = 0; i < len1; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = 0; j < len2 && j < n; j++)
+	{
+		concat[i + j] = s2[j];
+	}
+	concat[i + j] = '\0';
+	return (concat);
+}
+/**
+ * len - calculate the lenght of a string
+ *
+ * @str: string
+ *
+ * Return: lenght
+ */
+
+unsigned int len(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,4 +2,6 @@
 #define HOLBERTON
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+unsigned int len(char *str);
 #endif
