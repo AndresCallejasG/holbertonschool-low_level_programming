bafe56d0b2108156033f963d28033b32ee39519c
andrescallejasg <andres.callejasg@gmail.com>
2020-03-05 20:48:25 +0000

    reorder statements

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,20 +18,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *t2_ptr;
 	unsigned int i;
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != 0)
 	{
 		free(ptr);
 		return (0);
 	}
-
-	if (new_size == old_size)
-		return (ptr);
-
 	new_ptr = malloc(new_size);
+
 	if (new_ptr == 0)
 		return (0);
 	if (ptr == 0)
 		return (new_ptr);
+	if (new_size == old_size)
+		return (ptr);
 	t_ptr = new_ptr;
 	t2_ptr = ptr;
 	for (i = 0; i < new_size; i++)
