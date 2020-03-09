f201c1b2d3aced223c6377dc0bbe3e28a0acaf14
andrescallejasg <andres.callejasg@gmail.com>
2020-03-09 21:38:44 +0000

    different size

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,19 +18,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog = malloc(sizeof(struct dog));
 	if (ndog == 0)
 		return (0);
-
-	ndog->name = malloc(sizeof(char *));
-	if (ndog == 0)
+	for (i = 0; name[i] != '\0'; i++)
+	{
+	}	
+	ndog->name = malloc(sizeof(char)* i);
+	if (ndog->name == 0)
+	{
+		free(ndog);
 		return (0);
-
-	ndog->owner = malloc(sizeof(char *));
-	if (ndog == 0)
+	}
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+	}
+	ndog->owner = malloc(sizeof(char ) * i);
+	if (ndog->owner == 0)
+	{
+		free(ndog->name);
+		free(ndog);
 		return (0);
-
+	}
 	for (i = 0; name[i] != '\0'; i++)
 		ndog->name[i] = name[i];
+	ndog->name[i] = name[i];
 	for (i = 0; owner[i] != '\0'; i++)
 		ndog->owner[i] = owner[i];
+	ndog->owner[i] = owner[i];
 	ndog->age = age;
 
 	return (ndog);
