4dc923b954298ed93de73f6c6292c1b2b34669a2
andrescallejasg <andres.callejasg@gmail.com>
2020-03-09 17:45:26 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/**
+ * struct dog - simulates a dog
+ * @name: dog's name
+ * @age: it's age
+ * @owner: It's owner
+ *
+ * Description: :) Woof!
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
