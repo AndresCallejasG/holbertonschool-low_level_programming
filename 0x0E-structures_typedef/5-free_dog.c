8267754ffee522e97ccbe75dee78b04a97b852be
andrescallejasg <andres.callejasg@gmail.com>
2020-03-09 22:07:21 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a variable type struct dog
+ *
+ * @d: direction to dog
+ *
+ * Return: voiud
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
