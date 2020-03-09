8c673b75079557644bf29b49590dd652cfe67455
andrescallejasg <andres.callejasg@gmail.com>
2020-03-09 18:18:53 +0000

    null case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,13 +13,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == 0)
+	if (d == NULL)
 	{
 		d = malloc(sizeof(struct dog));
-		if (d == 0)
+		if (d == NULL)
 			return;
 	}
-	(*d).name = name;
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
