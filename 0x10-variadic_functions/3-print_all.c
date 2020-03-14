a406a2bf26257fa4773fb99381c0c8085a02ac10
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-14 22:17:56 +0000

    prints (almost) everything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * p_char - Prints a character
+ * @valist: va_list
+ * Return: none
+ **/
+void p_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ * p_float - Prints a float
+ * @valist: va_list
+ * Return: none
+ **/
+void p_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ * p_int - Prints a int
+ * @valist: va_list
+ * Return: none
+ **/
+void p_int(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+/**
+ * p_str - Prints a str
+ * @valist: va_list
+ * Return: none
+ **/
+void p_str(va_list valist)
+{
+	printf("%s", va_arg(valist, char *));
+}
+/**
+ * print_all - prints "anything"
+ * @format:  list of types of arguments
+ *
+ * Return: none
+ **/
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list valist;
+	char *separator;
+	p_func p_types[] = {
+		{"c", p_char},
+		{"f", p_float},
+		{"i", p_int},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+
+	va_start(valist, format);
+	i = 0;
+	separator = "";
+
+	while (format[i] != 0)
+	{
+		j = 0;
+		while (p_types[j].f != NULL)
+		{
+			if (p_types[j].tp[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				p_types[j].f(valist);
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(valist);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,21 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+/**
+ * struct print_types - Struct type
+ *
+ * @tp: format type
+ * @f: function
+ */
+struct print_types
+{
+	char *tp;
+	void (*f)();
+};
+typedef struct print_types p_func;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
