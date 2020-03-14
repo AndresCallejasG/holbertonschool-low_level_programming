7a2e2e090fbf4955a1a64182e86d1879d860f0be
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-14 18:35:52 +0000

    reorder statments to reduce if statemnts

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,3 @@
-#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 /**
@@ -19,10 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		if (separator != NULL)
-			printf("%d%s", va_arg(valist, int), separator);
-		else
-			printf("%d", va_arg(valist, int));
+		printf("%d", va_arg(valist, int));
+		if (separator != 0)
+			printf("%s", separator);
 	}
 	printf("%d\n", va_arg(valist, int));
 
