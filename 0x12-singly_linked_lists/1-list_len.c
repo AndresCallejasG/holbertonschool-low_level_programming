3133d785b7e08c72520cf8e92be4e4e3d855edd4
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-19 21:59:22 +0000

    lenght of a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len -  number of elements in a linked list_t list.
+ *
+ * @h: List
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	const list_t *current;
+
+	if (h)
+	{
+		current = h;
+
+		while (current)
+		{
+			current = current->next;
+			i++;
+		}
+		
+	}
+
+	return (i);	
+}
