58a129123444620be18fab5abe3702e1084064d3
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-20 00:53:58 +0000

    prints a message before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - prints a message
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
