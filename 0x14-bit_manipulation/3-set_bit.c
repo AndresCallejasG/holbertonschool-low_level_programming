9de0496daa61657ef79b6048a68b16bb529ae244
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-26 22:24:17 +0000

    sets the value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* get_bit - sets the value of a bit to 1 at a given index.
+*
+* @n: numb
+* @index: position from right to left
+*
+* Return: 1 if it worked, or -1 if an error occurred
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < sizeof(*n) * 8)
+	{
+		*n |= (1 << index);
+		return (1);
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,7 +1,10 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+
 #endif
