d64730a393fe5ca4bc011d063ec57a4a23c093ef
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-31 23:32:34 +0000

    open() instead of creat()

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename)
 	{
-		fd = creat(filename, 0600);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
 
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		/*empty file case*/
 		if (size == 0)
 			return (1);
-
+			
 		count = write(fd, text_content, size);
 		if (count == -1)
 			return (-1);
