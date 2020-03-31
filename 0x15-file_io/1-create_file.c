fe6ab30d87e11e925b0a1c86b3b435790179ad83
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-31 23:35:46 +0000

    empty file case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,13 +23,11 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 
+		/*empty file case*/
+		if (text_content == NULL)
+			return (1);
 		while (text_content[size] != 0)
 			size++;
-
-		/*empty file case*/
-		if (size == 0)
-			return (1);
-			
 		count = write(fd, text_content, size);
 		if (count == -1)
 			return (-1);
