8a2326a23ad1e40c71649ef4c33060b4bbe8dac2
andrescallejasg <andres.callejasg@gmail.com>
2020-05-03 23:33:38 +0000

    free dlist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that free a dlistint_t list.
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *aux;
+
+	if (head)
+	{
+		current = head->next;
+		while (current)
+		{
+			aux = current->next;
+			free(current);
+			current = aux;
+		}
+		free(head);
+	}
+}
