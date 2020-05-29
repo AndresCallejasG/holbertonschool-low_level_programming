608b935fd849e957721fd09ad5346b6cd2a3b7e3
andrescallejasg <andres.callejasg@gmail.com>
2020-05-29 21:01:03 +0000

    add node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,54 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -
+ * hash_table_set - adds an element to the hash table.
  *
- * Return: 
+ * @ht: hash table
+ * @key: node's key
+ * @value: node's value
+ *
+ * Return: 1 if succeeded or 0 if fails
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int idx;
-    hash_node_t *new_node, *current;
+	unsigned long int idx;
+	hash_node_t *new_node, *current;
 
+	if (!ht || !(ht->size))
+		return (0);
 
-    if(!ht || !(ht->size))
-        return(0);
+	if (strcmp(key, "") == 0)
+		return (0);
 
-    if(strcmp(key, "") == 0)
-        return(0);
+	idx = key_index((const unsigned char *)key, ht->size);
 
-    idx = key_index((const unsigned char *) key, ht->size);   
+	if (!(ht->array[idx]))
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		ht->array[idx] = new_node;
+	}
+	else
+	{
+		current = ht->array[idx];
+		while (current)
+		{
+			if (strcmp(current->key, key) == 0)
+			{
+				free(current->value);
+				current->value = strdup(value);
+				return (1);
+			}
+			current = current->next;
+		}
+		current = ht->array[idx];
+		new_node = malloc(sizeof(hash_node_t));
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new_node->next = current;
+		ht->array[idx] = new_node;
+	}
 
-    if (!(ht->array[idx]))
-    {        
-        new_node = malloc(sizeof(hash_node_t));
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        ht->array[idx] = new_node;        
-    }
-    else
-    {
-        current = ht->array[idx];
-        while(current)
-        {
-            if(strcmp(current->key, key) == 0)
-            {
-                free(current->value);
-                current->value = strdup(value);
-                return(1);
-            }
-            current = current->next;
-        }
-        current = ht->array[idx];
-        new_node = malloc(sizeof(hash_node_t));
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        new_node->next = current;
-        ht->array[idx] = new_node; 
-    }
-
-    return(1);
+	return (1);
 }
